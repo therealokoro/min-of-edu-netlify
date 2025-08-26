26c7b5f4d925fed3df6c280b968c720f10e79a9b
Okoro Redemption <okororedemption24@gmail.com>
2025-08-26 20:10:54 +0100

    filter out super admin from staff list

--- server/api/staffs/list.get.ts
+++ server/api/staffs/list.get.ts
@@ -1,10 +1,11 @@
 export default defineEventHandler(async (e) => {
   const query = getQuery<{ all?: boolean }>(e)
 
+  const superAdminEmail = String(process.env.SUPER_ADMIN_EMAIL)
   const queryObj = query.all ? {} : { role: "Staff" }
 
   const staffs = await prisma.user.findMany({
-    where: { ...queryObj },
+    where: { ...queryObj, AND: { email: { not: superAdminEmail } } },
     orderBy: { createdAt: "desc" }
   })
   return staffs
